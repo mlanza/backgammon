00096ab29e5fdad8c411d87ea4a39a2915b2e81b
Mario T. Lanza <me@mlanza.com>
2025-08-16 10:37:21 -0400

    expose moves

--- main.js
+++ main.js
@@ -4,11 +4,17 @@ import * as b from './backgammon.js';
 import {reg} from './libs/cmd.js';
 
 const $state = $.atom(b.init());
+const moves = _.pipe(_.deref, b.moves, _.toArray, $.log);
 
-reg({$state});
+reg({$state, moves});
 
 $.swap($state, b.roll([5, 6]));
 $.swap($state, b.move(11, 5));
 $.swap($state, b.move(11, 6));
 $.swap($state, b.commit());
-_.chain($state, _.deref, b.moves, _.toArray, $.log);
+$.swap($state, b.roll([6, 1]));
+$.swap($state, b.move(23, 6));
+$.swap($state, b.move(5, 1));
+$.swap($state, b.commit());
+$.swap($state, b.roll([3, 4]));
+$.swap($state, b.move(24, 3));
