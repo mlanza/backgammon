b81ff738f95540b923a3a6565ff2d51e43324a08
Mario T. Lanza <me@mlanza.com>
2025-08-26 18:33:06 -0400

    add zed debugging

--- libs/cmd.js
+++ libs/cmd.js
@@ -1,10 +1,9 @@
 import _ from "./atomic_/core.js";
 import $ from "./atomic_/shell.js";
-import dom from "./atomic_/dom.js";
 import imm from "./atomic_/immutables.js";
 
 export const registry = {};
-const params = new URLSearchParams(location.search);
+const params = new URLSearchParams(globalThis.location ? globalThis.location.search : "?monitor=*");
 const monitor = _.maybe(params.get("monitor"), _.split(_, ","));
 const nomonitor = _.maybe(params.get("nomonitor"), _.split(_, ","));
 
@@ -48,6 +47,6 @@ export const cmd = _.overload(cmd1, cmd1, cmd3, cmd3);
 
 export default cmd;
 
-reg({_, $, dom, imm});
+reg({ _, $, imm });
 
 Object.assign(globalThis, {cmd});
